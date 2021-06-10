3700d2b619521651bd090a02a4f343e0cf32ca94
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2021-06-10 14:15:19 +0800

    remove header.py

--- header.py
+++
@@ -1,9 +0,0 @@
-import sys
-sys.path.append("manim")
-from manimlib import *
-
-def eqRange(fromValue, toValue, step = 1):
-    value = fromValue
-    while(value <= toValue):
-        yield value
-        value += step
