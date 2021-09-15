d413b46bb1c99741277f837c8fe95648cf2c8e63
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2021-09-15 22:13:20 +0800

    完善ybkc图形

--- phy_ybkc_lxcwq.py
+++ phy_ybkc_lxcwq.py
@@ -2,12 +2,15 @@ import sys
 sys.path.append(".")
 import header as h
 from manimlib import *
+import math as m
 
 class Ybkc(VGroup):
     def __init__(self):
+        def var_scale(v):
+            return v / 4
         def vertex_scale(v):
             for i in range(0, len(v)):
-                    v[i] /= 4
+                    v[i] = var_scale(v[i])
             return v
         def array_scale(a):
             for vertex in a:
@@ -20,16 +23,27 @@ class Ybkc(VGroup):
             [-0.8, 1.35, 0],
             [0.1, 1.35, 0],
             [0.1, 0.4, 0],
-            [-1.4, 0.4, 0],
-            [-1.4, 0.85, 0],
+            [-1.45, 0.4, 0],
+            [-1.45, 0.85, 0],
             [-0.8, 3, 0]
         ]
-        poly_back_curve = VMobject()
+        poly_back_curve = VMobject(plot_depth = -1)
         poly_back_curve.set_points_as_corners(array_scale(poly_back_vertexs))
-        poly_back_curve.data["points"][-2] = vertex_scale([-1.4, 2.1, 0])
-
-        poly_back_curve.set_fill("#777777", opacity = 1).set_stroke(opacity = 0)
+        poly_back_curve.data["points"][-2] = vertex_scale([-1.45, 2.1, 0])
+        poly_back_curve.set_fill("#828282", opacity = 1).set_stroke(opacity = 0)
         self.back = poly_back_curve
+
+        # rdepth
+        poly_rdepth_vertexs = [
+            [0, 0.1, 0],
+            [22.15, 0.1, 0],
+            [22.15, -0.1, 0],
+            [0, -0.1, 0]
+        ]
+        poly_rdepth_curve = VMobject(plot_depth = -1)
+        poly_rdepth_curve.set_points_as_corners(array_scale(poly_rdepth_vertexs))
+        poly_rdepth_curve.set_fill("#858585", opacity = 1).set_stroke(opacity = 0)
+        self.rdepth = poly_rdepth_curve
         
         # body
         poly_vertexs = [
@@ -53,20 +67,21 @@ class Ybkc(VGroup):
         poly_curve.set_fill("#999999", opacity = 1).set_stroke(opacity = 0)
         self.body = poly_curve
 
+        # lmt
+        lmt1 = Rectangle(var_scale(0.8), var_scale(0.45), plot_depth = 1).move_to(vertex_scale([2.45, 1.825, 0]))
+        lmt1.set_fill("#bbbbbb", opacity = 1).set_stroke(opacity = 0)
+        lmt2 = Rectangle(var_scale(0.4), var_scale(0.25), plot_depth = 0).move_to(vertex_scale([2.45, 1.475, 0]))
+        lmt2.set_fill("#a4a4a4", opacity = 1).set_stroke(opacity = 0)
+        self.lmt = VGroup(lmt1, lmt2)
+
         # slider (up)
-        poly_slider_up_vertexs = [
-            [0.1, 1.35, 0],
-            [5.5, 1.35, 0],
-            [5.5, 0.4, 0],
-            [0.1, 0.4, 0],
-            [0.1, 1.35, 0]
-        ]
-        poly_slider_up_curve = VMobject()
-        poly_slider_up_curve.set_points_as_corners(array_scale(poly_slider_up_vertexs))
-        poly_slider_up_curve.set_fill("#bbbbbb", opacity = 1).set_stroke(opacity = 0)
-        self.poly_slider_up = poly_slider_up_curve
+        poly_slider_up = Rectangle(var_scale(5.4), var_scale(0.95), plot_depth = 1).move_to(vertex_scale([2.8, 0.875, 0]))
+        poly_slider_up.set_fill("#bbbbbb", opacity = 1).set_stroke(opacity = 0)
+        self.slider_up = poly_slider_up
 
         # slider (down)
+        sqrt2 = m.sqrt(2)
+        psd_cr_scale = 1.15
         poly_slider_down_vertexs = [
             [4.2, -1.35, 0],
             [1.35, -1.35, 0],
@@ -77,20 +92,47 @@ class Ybkc(VGroup):
             [0.1, -0.4, 0],
             [5.5, -0.4, 0],
             [5.5, -1.35, 0],
-            [5.1, -1.35, 0]
+            [5.1, -1.35, 0],
+            [4.65 + 0.45 / sqrt2, -1.35 - 0.45 / sqrt2, 0],
+            [4.65, -1.8, 0],
+            [4.65 - 0.45 / sqrt2, -1.35 - 0.45 / sqrt2, 0],
+            [4.2, -1.35, 0]
         ]
-        poly_slider_down_curve = VMobject()
+        poly_slider_down_curve = VMobject(plot_depth = 1)
         poly_slider_down_curve.set_points_as_corners(array_scale(poly_slider_down_vertexs))
+        for point in poly_slider_down_curve.data["points"][-2:-12:-3]:
+            point -= vertex_scale([4.65, -1.35, 0])
+            point *= psd_cr_scale
+            point += vertex_scale([4.65, -1.35, 0])
         poly_slider_down_curve.set_fill("#bbbbbb", opacity = 1).set_stroke(opacity = 0)
-        self.poly_slider_down = poly_slider_down_curve
+        self.slider_down = poly_slider_down_curve
         
-        super().__init__(self.back, self.body, self.poly_slider_up, self.poly_slider_down)
+        super().__init__(self.back, self.rdepth, self.body, self.lmt, self.slider_up, self.slider_down)
     
     def sliders(self):
-        return [self.back, self.poly_slider_up, self.poly_slider_down]
+        return [self.back, self.rdepth, self.lmt, self.slider_up, self.slider_down]
+    
+    def in_animate(self):
+        return AnimationGroup(
+            FadeIn(self.back, DOWN), FadeIn(self.body, RIGHT),
+            FadeIn(self.lmt, DOWN), FadeIn(self.slider_up, DOWN), FadeIn(self.slider_down, UP)
+            )
+    
+    def out_animate(self):
+        return AnimationGroup(
+            FadeOut(self.back, UP), FadeOut(self.body, LEFT),
+            FadeOut(self.lmt, UP), FadeOut(self.slider_up, UP), FadeOut(self.slider_down, DOWN)
+            )
 
 class Test(Scene):
     def construct(self):
-        ybkc = Ybkc()
-        self.play(FadeIn(ybkc, UP))
-        # self.play(*[mobj.animate.shift(RIGHT * 2) for mobj in ybkc.sliders()])
+        ybkc = Ybkc().shift(LEFT * 2)
+        self.play(ybkc.in_animate())
+        self.wait(0.5)
+        self.play(*[mobj.animate.shift(RIGHT * 2) for mobj in ybkc.sliders()])
+        self.wait(0.5)
+        self.play(*[mobj.animate.shift(LEFT * 2) for mobj in ybkc.sliders()])
+        # self.wait(0.2)
+        # self.remove(ybkc.rdepth)
+        # self.play(ybkc.out_animate())
+        
