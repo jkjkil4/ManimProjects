70dc9f6680f9359a4b430dbeca2abf1c6eef9643
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2021-10-05 22:56:26 +0800

    PhyEquip(Txt、Light)

--- phy/phy_header.py
+++ phy/phy_header.py
@@ -1,1 +1,46 @@
 from manimlib import *
+
+class PhyEquip(VMobject):
+    CONFIG = {
+        "radius": 1
+    }
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        circle_points = Arc.create_quadratic_bezier_points(TAU, n_components = 64)
+        self.append_points(circle_points * 0.9)
+        self.append_points(circle_points)
+        self.scale(self.radius).set_stroke("#eeeeee", width = 1).set_fill(opacity = 1)
+
+class PhyEquipTxt(PhyEquip):
+    def __init__(self, txt, txtscale = 1, **kwargs):
+        super().__init__(**kwargs)
+
+        self.txt = txt = Text(txt)
+        distance = np.sqrt(txt.get_right()[0] ** 2 + txt.get_top()[1] ** 2)
+        r = self.get_right()[0] * 0.65
+        txt.scale(r / distance * txtscale)
+        self.add(txt)
+
+class PhyEquipVMobj(PhyEquip):
+    def __init__(self, vmobj: VMobject, objscale = 1, **kwargs):
+        super().__init__(**kwargs)
+
+        maxdistance = max([np.sqrt(p[0] ** 2 + p[1] ** 2) for p in vmobj.get_all_points()])
+        r = self.get_right()[0] * 0.95
+        vmobj.scale(r / maxdistance * objscale)
+        self.vmobj = vmobj
+        self.add(vmobj)
+
+class PhyEquipLight(PhyEquipVMobj):
+    def __init__(self, **kwargs):
+        rect = Rectangle(1, 0.06).set_stroke(width = 0).set_fill(opacity = 1)
+        self.vg = VGroup(rect.copy().rotate(PI / 4), rect.copy().rotate(-PI / 4))
+        super().__init__(self.vg, **kwargs)
+
+class TestScene(Scene):
+    def construct(self):
+        a = PhyEquipLight()
+        b = PhyEquipTxt("V").next_to(a, LEFT)
+        c = PhyEquipTxt("A").next_to(a, RIGHT)
+        self.add(a, b, c)
