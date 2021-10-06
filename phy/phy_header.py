7b35685c0ca6598516f73d8651da652bac5d5206
jkjkil4 <1173374788@qq.com>
2021-10-06 09:52:01 +0000

    PhyMaterialEquip

--- phy/phy_header.py
+++ phy/phy_header.py
@@ -38,7 +38,26 @@ class PhyEquipLight(PhyEquipVMobj):
         self.vg = VGroup(rect.copy().rotate(PI / 4), rect.copy().rotate(-PI / 4))
         super().__init__(self.vg, **kwargs)
 
-class TestScene(Scene):
+# 未测试
+class PhyMaterialEquip(VGroup):
+    def __init__(self, txt = "", arc_radius = 1, arc_rad = PI / 2, grad_half_len = 0.1):
+        self.txt = Text(txt).scale(0.2).move_to([0, arc_radius / 2, 0])
+        arc_start_angle = (PI - arc_rad) / 2
+        self.arc = Arc(arc_start_angle, arc_rad, radius = arc_radius)
+        self.grads = VGroup()
+        for i in range(0, 41):
+            rot = arc_start_angle + (40 - i) / 40 * arc_rad
+            x = np.cos(rot) * arc_radius
+            y = np.sin(rot) * arc_radius
+            ghl = grad_half_len * (0.5 if i % 10 == 0 else (0.75 if i % 5 == 0 else 1))
+            xoffset = np.cos(rot) * ghl
+            yoffset = np.sin(rot) * ghl
+            self.grads.add(Line([x + xoffset, y + yoffset, 0], [x - xoffset, y - yoffset, 0]))
+
+        super().__init__(self.txt, self.arc, self.grads)
+
+
+class PhyHeaderTestScene(Scene):
     def construct(self):
         a = PhyEquipLight()
         b = PhyEquipTxt("V").next_to(a, LEFT)
