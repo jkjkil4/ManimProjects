9d83efb5c7493b5c4a17cce4b975506d627b3f33
jkjkil4 <52841865+jkjkil4@users.noreply.github.com>
2021-11-14 23:30:02 +0800

    小修改

--- phy/phy_elec_f.py
+++ phy/phy_elec_f.py
@@ -3,20 +3,6 @@ sys.path.append('.')
 from manimlib import *
 from header import *
 
-'''
-在 scene.py->Scene->progress_through_animations 中添加了如下一段：
-
-if self.auto_sort_by_camera_distance:
-    # 计算参照平面法向量
-    theta, phi = self.camera.frame.get_euler_angles()[0:2]
-    fvec = np.array([np.cos(theta), np.sin(theta), 1 / np.cos(phi)])
-    fvec /= np.sqrt(sum([v**2 for v in fvec]))
-    # 排序函数
-    def asbcd_cmp(a: Mobject):
-        return sum([v1 * v2 for v1, v2 in zip(a.get_center(), fvec)]) 
-    self.mobjects.sort(key = asbcd_cmp)
-'''
-
 class FaceTakedCube(Cube):
     CONFIG = { 
         "take_faces": (0, 0, 0, 0, 0, 0)
@@ -46,10 +32,6 @@ class FaceTakedCube(Cube):
             ])
 
 class TestScene(Scene):
-    CONFIG = {
-        "auto_sort_by_camera_distance": True,
-    }
-
     def construct(self):        
         frame = self.camera.frame
 
@@ -58,8 +40,8 @@ class TestScene(Scene):
             phi = 90 * DEGREES,
             gamma = -90 * DEGREES
         ).set_width(frame.get_width() * 1.2)
-        frame.focal_distance = 50
-        focal_distance_tracker = ValueTracker(50)
+        frame.focal_distance = 100
+        focal_distance_tracker = ValueTracker(100)
         def frame_updater(m):
             m.focal_distance = focal_distance_tracker.get_value()
         frame.add_updater(frame_updater)
@@ -70,7 +52,7 @@ class TestScene(Scene):
         cubeS = FaceTakedCube(color = BLUE, gloss = 0, shadow = 0, take_faces = (0, 0, 0, 0, 0, 1))\
             .next_to(ORIGIN, IN, buff = 3).stretch(2, 0).stretch(2, 1)
         self.add(cubeN, cubeS, ThreeDBorder(cubeN, color = BLACK, width = 0.025), ThreeDBorder(cubeS, color = BLACK, width = 0.025))
-        self.add(Arrow(OUT * 3, IN * 3))
+        self.add(Arrow(OUT * 3, IN * 3).apply_depth_test())
         self.wait()
         self.play(
             focal_distance_tracker.animate.set_value(2),
